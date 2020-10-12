e8871ea48ce101a159146541ed0987d7123a13aa
Osong Agberndifor <agberndifor7@gmail.com>
2020-10-12 22:18:22 +0100

    Add style to calculator app

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button type="button">
+const Button = ({ name, color, wide }) => (
+  <button type="button" className="btn" style={{ background: color, width: wide ? '50%' : '25%' }}>
     {name}
   </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: 'orange',
 };
 
 export default Button;
